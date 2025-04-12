319584ec38be21d537850129e1c0ae35480af611
Johan Mora <johanmora200412@gmail.com>
2025-04-11 20:10:21 -0500

    gh pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base:'https://mora676.github.io/Parcial_segundo_corte',
+});
