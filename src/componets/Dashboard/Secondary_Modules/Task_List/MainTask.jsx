ec61ec97ed5d7f8671d14c156f692a2f281c3c74
Johan Mora <johanmora200412@gmail.com>
2025-04-24 17:25:07 -0500

    file publishing paths to git hub pages

--- src/componets/Dashboard/Secondary_Modules/Task_List/MainTask.jsx
+++ src/componets/Dashboard/Secondary_Modules/Task_List/MainTask.jsx
@@ -6,6 +6,9 @@ import { Link, useParams } from 'react-router-dom';  // Importa useParams para a
 // import AddTaskModal from './AddTaskModal';
 
 function MainTask() {
+  // Apartado para el funcionamiento de las ventanas
+  const [activeTab, setActiveTab] = useState('tab1');
+
   const [tasks, setTasks] = useState([]);
   const [filteredTasks, setFilteredTasks] = useState([]);  // Estado para las tareas filtradas
   const [showModal, setShowModal] = useState(false);
@@ -13,48 +16,53 @@ function MainTask() {
   // Obtener el parámetro de la ruta (completadas, pendientes, todas)
   const { status } = useParams();
 
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/todos')
-      .then((res) => {
-        setTasks(res.data);  // Cargar todas las tareas
-      })
-      .catch(() => {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: 'No se pudieron cargar las tareas.',
-        });
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos')
+  //     .then((res) => {
+  //       setTasks(res.data);  // Cargar todas las tareas
+  //     })
+  //     .catch(() => {
+  //       Swal.fire({
+  //         icon: 'error',
+  //         title: 'Error',
+  //         text: 'No se pudieron cargar las tareas.',
+  //       });
+  //     });
+  // }, []);
 
 
   // Función para agregar una tarea
-  function addTask(title) {
-    const newTask = {
-      id: tasks.length + 1,
-      title: title,
-      completed: false,
-    };
-    setTasks([newTask, ...tasks]);
-    setShowModal(false);
+  // function addTask(title) {
+  //   const newTask = {
+  //     id: tasks.length + 1,
+  //     title: title,
+  //     completed: false,
+  //   };
+  //   setTasks([newTask, ...tasks]);
+  //   setShowModal(false);
 
-    Swal.fire({
-      icon: 'success',
-      title: 'Tarea agregada',
-      text: `Se agregó "${title}" correctamente.`,
-    });
-  }
+  //   Swal.fire({
+  //     icon: 'success',
+  //     title: 'Tarea agregada',
+  //     text: `Se agregó "${title}" correctamente.`,
+  //   });
+  // }
 
   return (
-   <div className='main-task'>
+    <div className='main-task'>
       <div className='main-task-options'>
-        <button className='tab btn1'></button>
-        <button className='tab btn2'></button>
+        <button
+          className={`tab-btn ${activeTab === 'active' ? 'desactive+' : ''}`}
+          onClick={() => setActiveTab('tab1')}
+        >
+          Pestaña 1
+        </button>
+        <button className='tab-btn btn2'></button>
       </div>
       <div className='main-task-boxs'>
         <div className='main-task-container'>comtemidp</div>
       </div>
-   </div>
+    </div>
 
   );
 }
